f193acd40e4fe27a8fa5ec5abf52c82b6088d87d
Ismael9876 <77596446+Ismael9876@users.noreply.github.com>
2021-02-27 20:22:55 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+var wall1;
+var wall2;
+var wall3;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
@@ -6,15 +9,29 @@ const Body = Matter.Body;
 
 function preload()
 {
-	
+
 }
 
 function setup() {
 	createCanvas(800, 700);
 
-
+  
+  
+  wall1 = createSprite(780, 600, 20, 110);
+  wall2 = createSprite(680, 645, 190, 20);
+  wall3 = createSprite(590, 600, 20, 110);
+  
 	engine = Engine.create();
 	world = engine.world;
+
+  var option = {
+    isStatic: false,
+    restitution: 3.0,
+    friction: 0.5,
+    dencity: 1.2,
+
+  }
+
 
 	//Create the Bodies Here.
 
@@ -27,10 +44,22 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+
+  ellipse(130, 645, 55, 55);
   
+  wall1.shapeColor = ("white");
+  wall2.shapeColor = ("white");
+  wall3.shapeColor = ("white");
+  
+  
+
   drawSprites();
  
 }
 
+function Pressed(){
+if (keyCode === UP_ARROW){
+  Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+}
 
-
+}
